7237f14ca3286b7fdc96a78d52c3db38d443cb0f
BmAchiraForWork <145047852+BmAchiraForWork@users.noreply.github.com>
2024-07-29 15:22:18 +0700

    2nd

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import streamlit as st
 from PIL import Image
 
+# Set the title of the Streamlit app
 st.title('คู่มือการใช้งาน')
 
-image = Image.open('how2.png')
-st.image(image)
+# Loop through the image names and display each one
+for i in range(1, 10):
+    image_path = f'how{i}.png'
+    image = Image.open(image_path)
+    st.image(image)
